f5c60f26dbcf484f5702cd2bf19731d0918c0fa5
wenjianli <wenjianli@umail.ucsb.edu>
2023-09-06 20:26:10 -0700

    added create user api v0

--- src/utils/clinic_check.ts
+++ src/utils/clinic_check.ts
@@ -1,6 +1,6 @@
 import {headers} from "next/headers";
 
-export const getClinic=()=>{
+export const getClinic=():string|null=>{
     const host=headers().get("host");
     if(host.endsWith("3000"))return "test";
     return null;
---
+++ src/utils/roles.ts
@@ -0,0 +1,6 @@
+
+/// the infomation for each roles corresponding to role number in user's info
+// id: collection id used in firebase + algolia
+// name: name displaying on UI
+export const roles=[{id:"ed",name:"Egg Donor"},{id:"recipient",name:"Recipient"},{id:"agency",name:"Clinics"}];
+
