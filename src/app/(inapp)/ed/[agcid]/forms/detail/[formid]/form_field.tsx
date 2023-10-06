d8db85513cd1a9caf593ae0d7b8fa4436d45633c
wenjianli <wenjianli@umail.ucsb.edu>
2023-10-06 16:48:29 -0700

    x

--- src/app/(inapp)/ed/[agcid]/forms/detail/[formid]/form_field.tsx
+++ src/app/(inapp)/ed/[agcid]/forms/detail/[formid]/form_field.tsx
@@ -1,5 +1,10 @@
 import { FormField } from "@/utils/form/template";
+import { Box, Input, Stack, Typography } from "@mui/material";
 
 export default function FormField({data}:{data:FormField}){
-    return 
+    return <Stack>
+        <Typography variant="body1">{data.label}</Typography>
+        <Box height={32}/>
+        {data.type==='text'?<Input></Input>}
+    </Stack>
 }
