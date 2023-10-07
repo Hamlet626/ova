5d20a78aae5bf60cebf7f38b29cc79288ddd9d3a
wenjianli <wenjianli@umail.ucsb.edu>
2023-10-06 18:32:52 -0700

    form content v0

--- src/app/(inapp)/ed/[agcid]/forms/detail/[formid]/form_field.tsx
+++ src/app/(inapp)/ed/[agcid]/forms/detail/[formid]/form_field.tsx
@@ -1,10 +1,14 @@
 import { FormField } from "@/utils/form/template";
-import { Box, Input, Stack, Typography } from "@mui/material";
+import { Box, FormHelperText, Input, Stack, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 export default function FormField({data}:{data:FormField}){
+    const {control, formState:{errors} }=useForm();
     return <Stack>
         <Typography variant="body1">{data.label}</Typography>
         <Box height={32}/>
-        {data.type==='text'?<Input></Input>}
+        ...{data.type==='text'?[<Input required={data.required} defaultValue={data.default}/>,
+        <FormHelperText error>Please fill in valid value</FormHelperText>]
+        :[]}
     </Stack>
 }
