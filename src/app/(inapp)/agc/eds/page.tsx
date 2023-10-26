1c595b4ad3c13ed011e1a59f10a9c537d2b9242c
wenjianli <wenjianli@umail.ucsb.edu>
2023-10-26 14:14:59 -0700

    x

--- src/app/(inapp)/agc/eds/page.tsx
+++ src/app/(inapp)/agc/eds/page.tsx
@@ -1,7 +1,21 @@
-import { Stack } from "@mui/material";
+'use client'
+import { font3 } from "@/components/ThemeRegistry/theme_consts";
+import { algo_client } from "@/utils/algolia";
+import { Stack, Typography } from "@mui/material";
+import { InstantSearchNext } from "react-instantsearch-nextjs";
+import { Autocomplete } from "./searcher";
 
 export default async function EDs(){
-    return <Stack px={10}>
-        
-    </Stack>
+    return <InstantSearchNext indexName="ed" searchClient={algo_client}>
+        <Stack px={10}>
+            <Stack direction={'row'} spacing={3}>
+                <Typography sx={font3} flexBasis={2}>Egg Donor</Typography>
+                <Autocomplete
+                placeholder="Search products"
+                detachedMediaQuery="none"
+                openOnFocus
+                />
+            </Stack>
+        </Stack>
+    </InstantSearchNext>;
 }
---
+++ src/app/(inapp)/agc/layout.tsx
@@ -0,0 +1,11 @@
+import { EDRcpAppBarMenu } from "@/components/layouts/ed_rcp";
+import { RoleNum } from "@/utils/roles";
+
+
+export default async function AGCLayout({children, params}: { children: React.ReactNode, params: { agcid: string } }) {
+    return(
+        <EDRcpAppBarMenu role={RoleNum.ED} agcid={params.agcid}>
+            {children}
+        </EDRcpAppBarMenu>
+    )
+}
