1a9017d63be3ac64fbc7163e05266b0fb13636cf
zhanchaoyang <zhanchaoyang1@gmail.com>
2023-11-09 20:23:07 -0700

    card for checkbox style

--- src/app/(inapp)/ed/[agcid]/forms/detail/[formid]/form_field.tsx
+++ src/app/(inapp)/ed/[agcid]/forms/detail/[formid]/form_field.tsx
@@ -6,15 +6,29 @@ import {typedLists} from "@/utils/form/consts";
 // import { languages,eyeColors,hairColors,ethnicities,nationalities,countryList,physicalTraits,dominantHands,} from "@/utils/form/consts";
 import React, { useState } from 'react';
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import {primary} from  "@/components/ThemeRegistry/theme";
+
+
 
 export default function FormFieldUI({data,register}:{data:FormField,register:any}){
 
-const fieldHeight = data.length === 'long' ? '3' : 'auto';
+const fieldHeight = data.length === 'long' ? '3' : '1';
+   const [cardStates, setCardStates] = useState({});
+
+   const handleCardClick = (value) => {
+     setCardStates((prevStates) => ({
+       ...prevStates,
+       [value]: !prevStates[value],
+     }));
+   };
 
 
-
-        console.log(data.length);
-        console.log(data.options);
+//
+//         console.log(data.length);
+//         console.log(data.options);
 const [formData, setFormData] = useState([{ language: '', fluency: 'Fluent' }]);
 const handleChange = (index, field, value) => {
   setFormData((prevData) => {
@@ -41,11 +55,10 @@ const handleChange = (index, field, value) => {
 
 
 
-    return <Stack key={data.id} mb={4} sx={{   }}>
+    return <Stack key={data.id} mb={4} >
         <Typography variant="body1">{data.label}</Typography>
         <Box height={8}/>
-        {data.type==='text'?[<TextField variant="outlined"   style={{ borderRadius: '50%',width: '100px',  height: '100px', padding: '16px',
-                                                             }}/>
+        {data.type==='text'?[<TextField multiline minRows={5}  variant="outlined"   />
 //         <FormHelperText error>Please fill in valid values</FormHelperText>
         ]
         :data.type==='date'?[<DateField variant="standard" />]
@@ -60,11 +73,29 @@ const handleChange = (index, field, value) => {
             <ToggleButton value={'yes'}>Yes</ToggleButton>
             <ToggleButton value={'no'}>No</ToggleButton>
         </ToggleButtonGroup>]
-        :data.type==='checkbox'?[<ToggleButtonGroup style={{ width: '500px', overflow: 'auto' }}>
-   {Array.isArray(typedLists[data.options]) ? typedLists[data.options].map((v) => <ToggleButton key={v} value={v}>{v}</ToggleButton>) : null }
-   {Array.isArray(data.options) ? data.options.map((v) => <ToggleButton key={v} value={v}>{v}</ToggleButton>) : null }
-
-      </ToggleButtonGroup>]
+        :data.type==='checkbox'?[<Box
+                                 display="flex"
+                                 flexWrap="wrap"
+                                     >
+                                       {Array.isArray(typedLists[data.options])
+                                         ? typedLists[data.options].map((v) => (
+                                             <Card variant='filled" ' key={v} value={v} sx={{ margin: 1,backgroundColor: cardStates[v] ? '#FFA07A' : 'grey',
+                                            cursor: 'pointer',
+=                                            }}
+                                           onClick={() => handleCardClick(v)}
+                                             >
+                                                 {v}
+                                             </Card>
+                                           ))
+                                         : null}
+                                       {Array.isArray(data.options)
+                                         ? data.options.map((v) => (
+                                             <Card key={v} value={v} sx={{ margin: 1 }}>
+                                                 {v}
+                                             </Card>
+                                           ))
+                                         : null}
+                                     </Box>]
 
         :data.type==='number'?[<TextField type='number' inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} />]
         :data.type==='address'?[<Input />,
@@ -72,16 +103,7 @@ const handleChange = (index, field, value) => {
         :data.type==='name'?[]
         :data.type==='populate'?[
 
-//        {data.group.map((groupData, i) => (
-//        <Select key={groupData.id}>
-//        {Array.isArray(typedLists[data.group[i].options]) &&
-//          typedLists[data.group[i].options].map((v) => (ç
-//           <MenuItem key={v} value={v}>
-//             {v}
-//           </MenuItem>
-//         )}
-//       </Select>
-//   )}
+
 
 
 
