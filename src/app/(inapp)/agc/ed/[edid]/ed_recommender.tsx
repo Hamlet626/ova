db898997b7b951bed1f2abf845518a0ca6d4a475
wenjianli <wenjianli@umail.ucsb.edu>
2023-11-19 20:41:57 -0800

    recommend v0.1 working

--- src/app/(inapp)/agc/ed/[edid]/ed_recommender.tsx
+++ src/app/(inapp)/agc/ed/[edid]/ed_recommender.tsx
@@ -4,11 +4,11 @@ import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Grid from '@mui/material/Grid';
-import {Typography} from '@mui/material';
+import {Chip, InputAdornment, Stack, Typography, alpha} from '@mui/material';
 import { debounce } from '@mui/material/utils';
 import { useConnector } from 'react-instantsearch';
 import connectAutocomplete from 'instantsearch.js/es/connectors/autocomplete/connectAutocomplete';
-import { EDRec, algo_client } from '@/utils/algolia';
+import { EDRec, RcpRec, algo_client } from '@/utils/algolia';
 import { roles, RoleNum } from '@/utils/roles';
 import { getAlgoliaResults } from '@algolia/autocomplete-js';
 
@@ -22,14 +22,66 @@ import {
 import {BaseItem} from '@algolia/autocomplete-shared/dist/esm/core'
 import { BaseSyntheticEvent, ReactNode, SyntheticEvent, useEffect, useMemo, useState } from 'react';
 import { useSession } from 'next-auth/react';
+import { neutral96 } from '@/components/ThemeRegistry/theme_consts';
+import { AddCircleOutline } from '@mui/icons-material';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import usePromise from 'react-use-promise';
+import { AgencyRef, UserRef, UsersAgcDataRef, app } from '@/utils/firebase/firebase_client';
+import { FieldValue, Firestore, arrayRemove, arrayUnion, collectionGroup, getDoc, getDocs, getFirestore, query, setDoc, where } from 'firebase/firestore';
+import { BasicInfoDoc } from '@/utils/firebase/types';
 
 export const EDRecommender=({edid}:{edid:string})=>{
-    const myid=useSession({required:true}).data?.user?.id;
-    return <UserSearcher agcid={''} role={RoleNum.ED} 
-    renderOption={(v: Hit<EDRec>)=> {
+    const myid='test'//useSession({required:true}).data?.user?.id!;
+    
+    const {data,setData}=useLocalPromise<BasicInfoDoc[]>(async()=>{
+      const r=await getDocs(query(collectionGroup(getFirestore(app),'agc data'),
+      where('agcid','==',myid),where('recommends','array-contains',edid)));
+      
+      return Promise.all(
+        r.docs.map(v=>getDoc(UserRef(RoleNum.Rcp,v.ref.parent.parent?.id!))
+        .then(v=>({uid:v.id,...v.data()}as any)))
+        );
+    },[edid,myid]);
+
+
+    return <Stack>
+      <GoogleMaps agcid={myid} role={RoleNum.Rcp} 
+      renderOption={(v: Hit<RcpRec>)=> {
         return <Typography>{v.name}</Typography>
-    } }/>
+        } }
+        onChange={async(ev,v,reason,detail)=>{
+          console.log(v,reason,detail);
+          if(v!=null&&reason==="selectOption"){
+            console.log(data,v.objectID)
+            const existed=data?.some((rcp)=>rcp.uid===v.objectID);
+            await setDoc(UsersAgcDataRef(RoleNum.Rcp,v.objectID,myid),
+            {recommends:existed?arrayRemove(edid):arrayUnion(edid)},
+            {merge:true});
+            setData((d)=>existed?d?.filter((rcp)=>rcp.uid!==v.objectID):[
+              ///todo:confirm
+              v as any,...d??[]]);
+          }
+        }}
+        />
+        <Box height={12}/>
+        <Grid2 container>
+          {(data??[]).map(v=><Chip label={v.name}/>)}
+        </Grid2>
+    </Stack>
 }
+
+const useLocalPromise=<T=any>(promise:Promise<T> | (() => Promise<T>),deps:any[]=[],defaultValue?:T,)=>{
+  const [data,setData]=useState<T|undefined>(defaultValue);
+  
+  const [pm,error,status]=usePromise(async()=>{
+    if(typeof promise === 'function'){
+      await promise().then(v=>setData(v));
+    }else await promise.then(v=>setData(v));
+  },deps);
+
+  return {data,setData,error,status};
+}
+
 export function UserSearcher<T extends BaseItem>({agcid,role,onChange,renderOption}:
     {agcid:string,role:RoleNum,
         onChange?:(event: SyntheticEvent, value: T | null, reason: string, details?: any) => void,
@@ -128,40 +180,18 @@ export default function GoogleMaps<T extends BaseItem>({agcid,role,onChange,rend
         onChange?:(event: SyntheticEvent, value: T | null, reason: string, details?: any) => void,
         renderOption:(v:T)=>ReactNode
     }){
-  const [value, setValue] = useState<T | null>(null);
   const [inputValue, setInputValue] = useState('');
   const [options, setOptions] = useState<readonly T[]>([]);
 
   const fetch = useMemo(
-    () =>
-      debounce(
-        async(
-          { input },
-          callback: (results?: readonly T[]) => void,
-        ) => {
-            getAlgoliaResults({
-                searchClient:algo_client,
-                queries: [
-                  {
-                    indexName: roles[role].id,
-                    query: input,
-                    params: {
-                      hitsPerPage:5,
-                    },
-                  },
-                ],
-              });
-              const algoData=await algo_client.initIndex(roles[role].id).search(input,{hitsPerPage:5});
+    () => debounce(
+        async( { input }, callback: (results?: readonly T[]) => void, ) => {
+          console.log('fetch',input);
+              const algoData=await algo_client.initIndex(roles[role].id).search(input,{
+                numericFilters:`agencies.${agcid}.status>=0`,
+                hitsPerPage:5});
+              console.log('result',algoData.hits);
                 return callback(algoData.hits as any[]);
-                return await Promise.all(algoData.hits.map(async v=>{
-                  const fbData=await getDoc(doc(getFirestore(app),UserDoc(RoleNum.ED,v.objectID)));
-                  //return {...v,...fbData.data};
-                  return {...v,...fbData.data} as AutocompleteItem;
-                }));
-        //   (autocompleteService.current as any).getPlacePredictions(
-        //     request,
-        //     callback,
-        //   );
         },
         400,
       ),
@@ -171,56 +201,54 @@ export default function GoogleMaps<T extends BaseItem>({agcid,role,onChange,rend
   useEffect(() => {
     let active = true;
 
-    if (inputValue === '') {
-      setOptions(value ? [value] : []);
-      return undefined;
-    }
+    // if (inputValue === '') {
+    //   setOptions(value ? [value] : []);
+    //   return undefined;
+    // }
 
     fetch({ input: inputValue }, (results?: readonly T[]) => {
+      console.log('active',active,inputValue,results);
       if (active) {
-        let newOptions: readonly T[] = [];
-
-        if (value) {
-          newOptions = [value];
-        }
-
-        if (results) {
-          newOptions = [...newOptions, ...results];
-        }
-
-        setOptions(newOptions);
+        setOptions(results??[]);
       }
     });
 
     return () => {
       active = false;
     };
-  }, [value, inputValue, fetch]);
+  }, [inputValue, fetch]);
 
   return (
     <Autocomplete
-      sx={{ width: 300 }}
-    //   filterOptions={(x) => x}
       options={options}
-    //   autoComplete
-    //   filterSelectedOptions
-      value={value}
+      filterOptions={(x) => x}
+      forcePopupIcon={false}
       noOptionsText="No locations"
       onChange={(event: any, newValue: T | null,reason,description) => {
-        setOptions(newValue ? [newValue, ...options] : options);
-        setValue(newValue);
         if(onChange!=null)onChange(event,newValue,reason,description);
       }}
+      getOptionLabel={(v:T)=>v.name}
       onInputChange={(event, newInputValue) => {
         setInputValue(newInputValue);
       }}
-      renderInput={(params) => (
-        <TextField {...params} label="Add a location" fullWidth />
-      )}
+      renderInput={(params) => ( 
+      <TextField
+      {...params}
+      placeholder="Add a location" fullWidth variant='standard'
+      InputProps={{ ...params.InputProps, disableUnderline: true ,
+      startAdornment:<InputAdornment position='start'><AddCircleOutline/></InputAdornment>}}
+      sx={{borderRadius: '28px',
+      backgroundColor: neutral96,
+      p:0.5,
+      '&:hover': {
+        backgroundColor: alpha(neutral96, 0.75),
+      },}}
+      />)
+    }
+      isOptionEqualToValue={(o,v)=>o.objectID===v.objectID}
       renderOption={(props, option) => {
-
         return (
-          <li {...props}>
+          <li {...props} key={`${option.objectID}`} >
             {renderOption(option)}
           </li>
         );
